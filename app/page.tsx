d1afb14046e493811a7289cef80ae8c136d7d32f
Adem ilter <ademilter@gmail.com>
2023-10-17 13:22:09 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <iframe
+            className="fixed inset-0 h-screen w-screen border-0"
+            allowFullScreen
+            src="https://raindrop.io/ademilter/prompt-share-38467538/embed/sort=-created&theme=auto&hide=excerpt%2C+note"/>
+    )
+}
