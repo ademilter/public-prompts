9f64cf067e443b4ca1be91483c4cb4d56268acb5
Adem ilter <ademilter@gmail.com>
2023-10-17 14:05:58 +0300

    fix submit button

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
         <main className="min-h-screen">
             <header className="text-center p-6">
                 <h1>Curated by @ademilter</h1>
+
+                <a className="underline text-blue-500" target="_blank" href="https://airtable.com/app7ZuB47ASMoCSHY/shrW5pQXlChBeJ4ik">Submit for review</a>
             </header>
             <iframe
                 className="border-0 w-full h-screen"
